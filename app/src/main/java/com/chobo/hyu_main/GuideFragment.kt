3e4575251ff300b74dabab5b57bf0f05dce0269e
rud15dns@naver.com <rud15dns@naver.com>
2024-11-01 22:55:18 +0900

    branch

--- app/src/main/java/com/chobo/hyu_main/GuideFragment.kt
+++ app/src/main/java/com/chobo/hyu_main/GuideFragment.kt
@@ -6,7 +6,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView // ImageView import 추가
-import androidx.fragment.app.FragmentManager // FragmentManager import 추가
 
 class GuideFragment : Fragment() {
 
